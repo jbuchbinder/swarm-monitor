11db667f78a708b8d52b889ee824116d168f5543
Jeff Buchbinder <jeffreymbuchbinder@gmail.com>
2013-08-21 18:14:27 -0400

    Add convenience function to initialize replacer with map.

---
+++ replacer.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"strings"
+)
+
+// Create a strings.Replacer object from a map of orig => replacement
+func ReplacerFromMap(args map[string]string) *strings.Replacer {
+	// Convert into array of 0 => key, 1 => val, 2 => key, etc...
+	arglist := make([]string, len(args)*2)
+	c := 0
+	for k, v := range args {
+		arglist[(c * 2)] = k
+		arglist[(c*2)+1] = v
+		c++
+	}
+	return strings.NewReplacer(arglist...)
+}
